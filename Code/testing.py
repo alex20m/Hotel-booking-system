5a18550ed8dff635cc5db927995ed2958f7d2021
alexm <alex.mecklin@aalto.fi>
2023-03-09 21:19:37 +0200

    Git commit

---
+++ Code/testing.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class TestReservations(unittest.TestCase):
+
+    def test_reservation_not_through(self):
+        pass
+
+    def test_reservation_through(self):
+        pass
+
+    def test_reservation_length(self):
+        pass
+
+    def test_price(self):
+        pass
+
+    def test_read_from_file(self):
+        pass
+
+    def test_write_to_file(self):
+        pass
+
