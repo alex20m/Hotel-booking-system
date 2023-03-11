0d8fa2a9c5aa8c27b051713f6b6fe7a9dd9bd593
alexm <alex.mecklin@aalto.fi>
2023-03-11 12:28:06 +0200

    Class created and functions made

---
+++ Code/room_type.py
@@ -0,0 +1,13 @@
+
+
+class RoomType:
+
+    def __init__(self, room_name, price):
+        self.room_name = room_name
+        self.price = price
+
+    def get_room_name(self):
+        return self.room_name
+
+    def get_price(self):
+        return self.price
