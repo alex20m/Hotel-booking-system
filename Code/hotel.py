bb30124e30db4e6aba154e17369296233d179a54
alexm <alex.mecklin@aalto.fi>
2023-03-11 19:19:48 +0200

    Check availability function working

--- Code/hotel.py
+++ Code/hotel.py
@@ -12,44 +12,54 @@ class Hotel:
     We use the guests phone numbers to identify the guests, since guests can have the same names, but
     they can't have the same phone number. A phone number is 10 digits and is a string.
 
-    hotel_reservations is a dictionary with all the hotels reservations. The key is a guest object
-    and the values are a list where the first element is the check in date, the second element is checkout date.
+    hotel_reservations is a list with all the hotels reservations. The elements are lists where the first element is the
+    guests phone number, and the second element is the name, the third element is email, fourth element room_type,
+    fifth element start_date, sixth end_date and seventh comments.
 
     hotel_guests is a dictionary where the key is the guests phone number and the value are the guest object.
     """
 
-    def __init__(self):
-        self.hotel_reservations = {}
+    def __init__(self, filename):
         self.hotel_guests = {}
+        self.hotel_reservations = self.read_previous_reservations(filename)
 
     def read_previous_reservations(self, file_name):
         try:
+            hotel_reservations = []
             file = open(file_name, "r")
             for line in file:
                 line = line.rstrip()
-                dic_split = line.split(":")
-                phone_nr = dic_split[0]
-                val_split = dic_split[1].split(";")
-                name = val_split[0]
-                email = val_split[1]
-                start_date = val_split[2]
+                split = line.split(";")
+                phone_nr = split[0]
+                name = split[1]
+                email = split[2]
+                start_date = split[3]
                 start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
-                end_date = val_split[3]
+                end_date = split[4]
                 end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-                room_type = val_split[4]
-                comments = val_split[5]
+                room_type = split[5]
+                comments = split[6]
 
                 if phone_nr not in self.hotel_guests:
                     guest = Guest(name, phone_nr, email)
                     self.hotel_guests[phone_nr] = guest
-                self.hotel_reservations[guest] = [start_date, end_date]
+                hotel_reservations.append([phone_nr, name, email, room_type, start_date, end_date, comments])
             file.close()
-            return True
+            return hotel_reservations
         except OSError:
             return False
 
-    def check_availability(self, start_date, end_date):
-        pass
+    def check_availability(self, start_date, end_date, room_type):
+        for reservation in self.hotel_reservations:
+            check_list = []
+            if reservation[3] == room_type:
+                if start_date >= reservation[5]:
+                    pass
+                elif end_date <= reservation[4]:
+                    pass
+                else:
+                    return False
+        return True
 
     def print_reservations_in_interval(self, start_date, end_date):
         pass
