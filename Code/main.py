553bd11598d5a03c0a790a2aa06d3c2e20f2775a
alexm <alex.mecklin@aalto.fi>
2023-03-12 15:00:24 +0200

    Main added for testing

---
+++ Code/main.py
@@ -0,0 +1,17 @@
+from datetime import date
+from hotel import Hotel
+
+
+def main():
+
+    check_in = date(2023, 3, 9)
+    check_out = date(2023, 3, 10)
+    name = "Alex"
+    email = "email@test.com"
+    comment = "-"
+    hotel = Hotel("hotel_reservations_test")
+    room_type = "Cheap room"
+    phone_nr = "112"
+    print(hotel.get_guest_reservations("112")[1].get_price())
+
+main()
